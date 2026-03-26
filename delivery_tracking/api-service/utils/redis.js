fc25764f70d3980926f8a75c6cf6f804a1c939d8
duttabikram <136303237+duttabikram@users.noreply.github.com>
2026-03-26 23:27:47 +0530

    Update redis.js

--- delivery_tracking/api-service/utils/redis.js
+++ delivery_tracking/api-service/utils/redis.js
@@ -1,8 +1,8 @@
 const { Redis } = require("@upstash/redis");
 
 const redis = new Redis({
-  url: "https://deciding-seasnail-20791.upstash.io",
-  token: "AVE3AAIncDI3NmIzODI2MjEwYzc0ZjI3YWU2YzJhNDhkYTQ4N2U2ZHAyMjA3OTE",
+  url: "https://flowing-buzzard-85322.upstash.io",
+  token: "gQAAAAAAAU1KAAIncDEyNjJkNTkyNWE0YWQ0YjdkOTBkNWQxYzNhYTNkNTIzM3AxODUzMjI",
 });
 
 module.exports = redis;
