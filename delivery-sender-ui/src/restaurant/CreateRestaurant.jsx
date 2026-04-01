4657289d27dd7266fe2c7d0517d768eda4e44e9c
Bikram Dutta <duttabikram53@gmail.com>
2026-04-02 01:38:39 +0530

    your message

--- delivery-sender-ui/src/restaurant/CreateRestaurant.jsx
+++ delivery-sender-ui/src/restaurant/CreateRestaurant.jsx
@@ -91,7 +91,7 @@ useEffect(() => {
   };
 
   return (
-    <div className="auth-page">
+    <div className="auth-pagee">
       <div className="overlay"></div>
       <div className="auth-card">
         <h2>🏪 Create Your Restaurant</h2>
