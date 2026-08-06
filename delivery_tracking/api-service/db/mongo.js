8e2ac17476fc8dd68829f8d20b3c679348edcb40
Bikram Dutta <duttabikram53@gmail.com>
2026-08-07 00:17:50 +0530

    Added restaurant creation feature

--- delivery_tracking/api-service/db/mongo.js
+++ delivery_tracking/api-service/db/mongo.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 
 async function connectMongo() {
-  const uri = "mongodb+srv://duttabikram53_db_user:vyxDWdXrs2QPgqX6@cluster12.ufw6rmj.mongodb.net/?appName=Cluster12";
-
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ MongoDB connection failed", err);
